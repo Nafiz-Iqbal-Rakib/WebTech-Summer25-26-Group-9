b9cf5e810890b8481966b29d8deb35646470ab2b
Nafiz Iqbal <nafiziqbalrakib@gmail.com>
2026-08-18 18:11:07 +0600

    Added reusable admin sidebar

---
+++ View/Admin/sidebar.php
@@ -0,0 +1,36 @@
+<!-- Admin dashboard sidebar -->
+<aside class="sidebar">
+    
+    <nav class="sidebar-nav">
+
+        <ul>
+        
+            <li><a href="\WebTech-Summer25-26-Group-9\View\Admin\adminDashboardPage.php"><i class="fas fa-border-all"></i> Dashboard</a></li>
+            <li><a href="\WebTech-Summer25-26-Group-9\View\Admin\adminUserPage.php"><i class="far fa-user"></i> Users</a></li>
+            <li><a href="\WebTech-Summer25-26-Group-9\View\Admin\adminProductPage.php"><i class="fas fa-box"></i> Products</a></li>
+            <li><a href="\WebTech-Summer25-26-Group-9\View\Admin\adminOrderPage.php"><i class="far fa-clipboard"></i> Orders</a></li>
+
+        </ul>
+    </nav>
+
+    <div class="sidebar-footer">
+
+        <div class="user-info">
+
+            <div class="avatar">A</div>
+            <div class="details">
+                <span class="name">Admin</span>
+                <span class="email">admin@arabi.com</span>
+            </div>
+        </div>
+
+        <a href="\WebTech-Summer25-26-Group-9\View\updateInfopage.php">
+            <i class="fas fa-user-edit"></i>
+        </a>
+
+        <a href="\WebTech-Summer25-26-Group-9\View\landingPage.php"> 
+            <i class="fas fa-sign-out-alt logout-btn"></i>
+        </a>
+
+    </div>
+</aside>
