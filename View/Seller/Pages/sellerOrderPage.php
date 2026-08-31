d2c3f79ab83f5da71acbc4dbbd2c90df4bb1abf6
SanjedaChaity <sanjedaalamjessore123@gmail.com>
2026-09-01 04:03:06 +0600

    Use relative CSS paths for seller pages

--- View/Seller/Pages/sellerOrderPage.php
+++ View/Seller/Pages/sellerOrderPage.php
@@ -16,7 +16,7 @@ $sellerOrderData = $sellerController->getSellerOrderData($userId);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Seller Portal - Orders</title>
-    <link rel="stylesheet" href="/WebTech-Summer25-26-Group-9/View/Seller/Designs/OrderStyle.css">
+    <link rel="stylesheet" href="../Designs/OrderStyle.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 </head>
 <body>
--- View/Seller/Pages/sellerProductPage.php
+++ View/Seller/Pages/sellerProductPage.php
@@ -16,7 +16,7 @@ $sellerData = $sellerController->getSellerProductData($userId);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Seller Portal - Products</title>
-    <link rel="stylesheet" href="/WebTech-Summer25-26-Group-9/View/Seller/Designs/ProductStyle.css">
+    <link rel="stylesheet" href="../Designs/ProductStyle.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 </head>
 <body>
