6b019192ac0cda4caaf3322db9cee6060e391458
Nafiz Iqbal <nafiziqbalrakib@gmail.com>
2026-09-01 15:28:44 +0600

    Fix image path for product display in ShopController

--- Controller/ShopController.php
+++ Controller/ShopController.php
@@ -31,7 +31,7 @@ class ShopController
                     $row["product_name"],
 
                 "image" =>
-                    "/WebTech-Summer25-26-Group-9/Uploaded%20File/" .
+                    "/WebTech-Summer25-26-Group-9/uploads/products/" .
                     $row["img"],
 
                 "price" =>
